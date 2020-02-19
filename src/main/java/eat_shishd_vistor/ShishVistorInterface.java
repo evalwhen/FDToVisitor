ed74934fe56025dad30a3100c2d693d09baf6123
HuJianfeng <huzi.master@outlook.com>
2020-02-19 17:29:46 +0800

    重构 Shish OnlyOnions and Isvegetarian Vistor.

---
+++ src/main/java/eat_shishd_vistor/ShishVistorInterface.java
@@ -0,0 +1,11 @@
+package eat_shishd_vistor;
+
+public interface ShishVistorInterface {
+  boolean forLamb(ShishD s);
+
+  boolean forOnion(ShishD s);
+
+  boolean forSkewer();
+
+  boolean forTomato(ShishD s);
+}
