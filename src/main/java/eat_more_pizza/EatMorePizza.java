67838ce1d295f8070bf1f67bc5af4155c6ba97da
HuJianfeng <huzi.master@outlook.com>
2020-02-19 15:21:41 +0800

    add more vistors.

--- src/main/java/eat_more_pizza/EatMorePizza.java
+++ src/main/java/eat_more_pizza/EatMorePizza.java
@@ -1,46 +1,18 @@
 package eat_more_pizza;
 
 abstract class PizzaPieD {
-  RemAV remFn = new RemAV();
-  RemFishV rfFn = new RemFishV();
-  RemIntV riFn = new RemIntV();
   RemV roFn = new RemV();
-
-  SubstFishV sfFn = new SubstFishV();
   SubstV soFn = new SubstV();
 
-  abstract PizzaPieD remA();
-
-  abstract PizzaPieD remFish(FishD f);
-
-  abstract PizzaPieD remInt(Integer i);
-
   abstract PizzaPieD Rem(Object o);
-
-  abstract PizzaPieD substFish(FishD n, FishD o);
 
   abstract PizzaPieD subst(Object n, Object o);
 }
 
 class Bottom extends PizzaPieD {
-  PizzaPieD remA() {
-    return remFn.forBottom();
-  }
-
-  PizzaPieD remFish(FishD f) {
-    return rfFn.forBottom(f);
-  }
-
-  public PizzaPieD remInt(Integer i) {
-    return riFn.forBottom(i);
-  }
 
   PizzaPieD Rem(Object o) {
     return roFn.forBottom(o);
-  }
-
-  PizzaPieD substFish(FishD n, FishD o) {
-    return sfFn.forBottom(n, o);
   }
 
   PizzaPieD subst(Object n, Object o) {
@@ -57,24 +29,8 @@ class Topping extends PizzaPieD {
     r = _r;
   }
 
-  PizzaPieD remA() {
-    return remFn.forTopping(t, r);
-  }
-
-  PizzaPieD remFish(FishD f) {
-    return rfFn.ForTopping(t, r, f);
-  }
-
-  public PizzaPieD remInt(Integer i) {
-    return riFn.forTopping(t, r, i);
-  }
-
   PizzaPieD Rem(Object o) {
     return roFn.forTopping(t, r, o);
-  }
-
-  PizzaPieD substFish(FishD n, FishD o) {
-    return sfFn.forTopping(t, r, n, o);
   }
 
   PizzaPieD subst(Object n, Object o) {
@@ -93,6 +49,6 @@ public class EatMorePizza {
         )
       );
 
-    p.substFish(new Tuna(), new Anchovy());
+    p.subst(new Tuna(), new Anchovy());
   }
 }
