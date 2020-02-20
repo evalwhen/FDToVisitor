c733bd36bc62b9e92defa33ccf41598d3c7fec62
HuJianfeng <huzi.master@outlook.com>
2020-02-20 12:37:20 +0800

    // 这里的覆盖是为了改变 父类 HasPtV 中的 forTrans 的行为，毕竟我们没有覆盖它本身   // 只通过覆盖此 newHasPt, 间接的改变了 forTrans 在 UnionHasPtV 中的意义。

--- src/main/java/be_a_good_visitor/UnionHasPtV.java
+++ src/main/java/be_a_good_visitor/UnionHasPtV.java
@@ -1,13 +1,19 @@
 package be_a_good_visitor;
 
 class UnionHasPtV extends HasPtV implements UnionVistorInterface {
-  public UnionHasPtV(PointD p) {
+  private ShapeD s;
+  private ShapeD t;
+
+  UnionHasPtV(PointD p) {
     super(p);
   }
 
+  // 这里的覆盖是为了改变 父类 HasPtV 中的 forTrans 的行为，毕竟我们没有覆盖它本身
+  // 只通过覆盖此 newHasPt, 间接的改变了 forTrans 在 UnionHasPtV 中的意义。
   @Override
   ShapeDVisitorInterface newHasPt(PointD p) {
-    return new UnionHasPtV(p);
+    UnionHasPtV unionHasPtV = new UnionHasPtV(p);
+    return unionHasPtV;
   }
 
   public boolean forUnion(ShapeD s, ShapeD t) {
